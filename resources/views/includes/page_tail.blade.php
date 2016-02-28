67583d382aef38aa20bd1a7a480415908b59375f
Jaume Sala <jaumesala@gmail.com>
2016-02-28 01:08:40 +0100

    Tail libraries

--- resources/views/includes/page_tail.blade.php
+++ resources/views/includes/page_tail.blade.php
@@ -1,8 +1,12 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="{{ asset("js/vendor/jquery.js") }}"><\/script>')</script>
-        
+
         <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.14.1/mapbox-gl.js'></script>
-        {{-- // <script src="https://www.google.com/recaptcha/api.js?hl={{ LaravelLocalization::getCurrentLocale() }}" async defer></script> --}}
+
+        <script src='https://api.mapbox.com/mapbox.js/plugins/turf/v2.0.2/turf.min.js'></script>
+
+        <!-- <script src='https://api.mapbox.com/mapbox.js/v2.3.0/mapbox.js'></script> -->
+        <!-- <script src='https://api.mapbox.com/mapbox.js/plugins/leaflet-omnivore/v0.2.0/leaflet-omnivore.js'></script> -->
 
         @if (App::environment() != 'production')
             <script src="{{ asset('js/plugins.js') }}"></script>

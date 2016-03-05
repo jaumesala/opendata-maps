c8fcad2d594c3959b5bcacf0cb83847ef528c602
Jaume Sala <jaumesala@gmail.com>
2016-03-05 20:44:58 +0100

    Main JS and CSS object files

---
+++ resources/assets/js/admin/app.js
@@ -0,0 +1,77 @@
+/* @preserve
+ *
+ * Author:  Jaume Sala
+ * Website: jaumesala.net
+ *
+ */
+
+
+/* ---------- SITE OBJECT ---------- */
+
+SITE = {
+    common: {
+        init: function() {
+            console.log('common');
+        }
+    },
+
+    auth: {
+        init: function() {
+            console.log('auth');
+        }
+    },
+
+    dashboard: {
+        init: function() {
+            console.log('dashboard');
+        }
+    },
+
+    settings: {
+        init: function() {
+            console.log('settings');
+        }
+    },
+
+    user: {
+        init: function() {
+            console.log('user');
+        }
+    },
+
+    map: {
+        init: function() {
+            console.log('map');
+        }
+    }
+
+}
+
+
+
+/* ---------------------------------------------------------------------------------------------------- */
+UTIL = {
+    exec: function(controller, action) {
+        var ns = SITE,
+            action = (action === undefined) ? "init" : action;
+
+        if (controller !== "" && ns[controller] && typeof ns[controller][action] == "function") {
+            ns[controller][action]();
+        }
+    },
+
+    init: function() {
+        var body = document.body,
+            controller = body.getAttribute("data-controller").trim(),
+            action = body.getAttribute("data-action").trim();
+
+        UTIL.exec("common");
+        UTIL.exec(controller);
+        UTIL.exec(controller, action);
+    }
+};
+/* ---------------------------------------------------------------------------------------------------- */
+
+
+//Let's go baby!
+$(document).ready(UTIL.init);

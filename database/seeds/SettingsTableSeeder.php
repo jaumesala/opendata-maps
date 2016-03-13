164e8a8844cafa3e5fda9a6b689fc7a84de2478b
Jaume Sala <jaumesala@gmail.com>
2016-03-13 17:04:22 +0100

    Add settigns default values

--- database/seeds/SettingsTableSeeder.php
+++ database/seeds/SettingsTableSeeder.php
@@ -18,13 +18,55 @@ class SettingsTableSeeder extends Seeder
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
                 ],
+            [   'group'  => 'mapbox',
+                'key' => 'mapboxStyles',
+                'value'  => 'https://api.tiles.mapbox.com/mapbox-gl-js/v0.14.2/mapbox-gl.css',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ],
+            [   'group'  => 'mapbox',
+                'key' => 'mapboxScripts',
+                'value'  => 'https://api.tiles.mapbox.com/mapbox-gl-js/v0.14.1/mapbox-gl.js',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ],
+            [   'group'  => 'mapbox',
+                'key' => 'turfScripts',
+                'value'  => 'https://api.mapbox.com/mapbox.js/plugins/turf/v2.0.2/turf.min.js',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ],
+            [   'group'  => 'maps',
+                'key' => 'defaultOrder',
+                'value'  => 'created_at',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ],
+            [   'group'  => 'maps',
+                'key' => 'pageResults',
+                'value'  => 12,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ],
+            [   'group'  => 'sources',
+                'key' => 'defaultOrder',
+                'value'  => 'created_at',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ],
+            [   'group'  => 'sources',
+                'key' => 'pageResults',
+                'value'  => 10,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ],
         );
 
         // Uncomment the below to run the seeder
         DB::table('settings')->insert($settings);
 
 
-        factory(App\Models\Setting::class, 10)->create();
+        // factory(App\Models\Setting::class, 10)->create();
 
 
     }

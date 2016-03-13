5b71545b9eba5295217c58242d1cee6df6b44648
Jaume Sala <jaumesala@gmail.com>
2016-03-13 17:02:34 +0100

    Fix identation

--- resources/views/admin/sections/dashboard/index.blade.php
+++ resources/views/admin/sections/dashboard/index.blade.php
@@ -3,25 +3,25 @@
 
 @section('content')
 
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <h1>
-            Dashboard
-            <small>Control Panel</small>
-        </h1>
-        <ol class="breadcrumb">
-            <li><a href="{{ route('admin.dashboard.index') }}"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Dashboard</li>
-        </ol>
-    </section>
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        Dashboard
+        <small>Control Panel</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="{{ route('admin.dashboard.index') }}"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+    </ol>
+</section>
 
-    <!-- Main content -->
-    <section class="content">
+<!-- Main content -->
+<section class="content">
 
-    <!-- Your Page Content Here -->
+<!-- Your Page Content Here -->
 
 
-    </section>
-    <!-- /.content -->
+</section>
+<!-- /.content -->
 
 @stop

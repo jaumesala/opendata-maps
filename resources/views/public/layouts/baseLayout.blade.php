e27ded8b489ba80b8c12a59e826d3bb33c18b2e2
Jaume Sala <jaumesala@gmail.com>
2016-03-13 16:59:43 +0100

    Move old files to subfolder

---
+++ resources/views/public/layouts/baseLayout.blade.php
@@ -0,0 +1,9 @@
+@include('public.includes.page_head')
+
+@include('public.components.header')
+
+@yield('content', 'content')
+
+@include('public.components.footer')
+
+@include('public.includes.page_tail')

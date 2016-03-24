db66c3be077996ebb6c8fdf48f03d4d46dd1cb27
Jaume Sala <jaumesala@gmail.com>
2016-03-24 17:55:51 +0100

    Map - Added mass assignable attributes

--- app/Models/Map.php
+++ app/Models/Map.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Map extends Model
 {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'description', 'status',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);

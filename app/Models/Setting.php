adbf534e71ca4bc23b72b5f3e0ccffa452ccccaf
Jaume Sala <jaumesala@gmail.com>
2016-04-02 14:31:09 +0200

    Settings - Remove timestamps columns

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Setting extends Model
 {
     /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array

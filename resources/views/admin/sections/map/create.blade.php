261515349914154839ff0ba4cc031692a225b594
Jaume Sala <jaumesala@gmail.com>
2016-04-02 14:41:11 +0200

    New line

--- resources/views/admin/sections/map/create.blade.php
+++ resources/views/admin/sections/map/create.blade.php
@@ -27,6 +27,7 @@
     <div class="row">
 
         <div class="col-sm-12 col-md-5 col-lg-4">
+
             <form method="POST" action="{{ route('admin.map.store') }}" >
                 {{ csrf_field() }}
 

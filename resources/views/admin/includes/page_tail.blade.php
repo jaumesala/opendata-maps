cbc199e65e77116fbdf2512782c23853638123b8
Jaume Sala <jaumesala@gmail.com>
2016-04-04 23:57:09 +0200

    Add jquery ui reference

--- resources/views/admin/includes/page_tail.blade.php
+++ resources/views/admin/includes/page_tail.blade.php
@@ -1,6 +1,10 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="{{ asset("js/vendor/jquery.js") }}"><\/script>')</script>
 
+        <!-- <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css"> -->
+        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+        <script>window.jQuery.ui || document.write('<script src="{{ asset("js/vendor/jquery-ui.js") }}"><\/script>')</script>
+
         <!-- Mapbox GL -->
         <script src='{!! setting_value('mapbox', 'glScript') !!}'></script>
         <!-- Turf -->

75fd0d96462b93c70db275345c28e394429faeb7
Jaume Sala <jaumesala@gmail.com>
2016-04-16 23:26:48 +0200

    Page tail - Refactor minification code

--- resources/views/admin/includes/page_tail.blade.php
+++ resources/views/admin/includes/page_tail.blade.php
@@ -1,9 +1,16 @@
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="{{ asset("js/vendor/jquery.js") }}"><\/script>')</script>
+        <?php
+            if(App::environment() != 'production')
+                $min = '';
+            else
+                $min = '.min';
+        ?>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery{{ $min }}.js"></script>
+        <script>window.jQuery || document.write('<script src="{{ asset("js/vendor/jquery".$min.".js") }}"><\/script>')</script>
 
         <!-- <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css"> -->
-        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-        <script>window.jQuery.ui || document.write('<script src="{{ asset("js/vendor/jquery-ui.js") }}"><\/script>')</script>
+        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui{{ $min }}.js"></script>
+        <script>window.jQuery.ui || document.write('<script src="{{ asset("js/vendor/jquery-ui".$min.".js") }}"><\/script>')</script>
 
         <!-- Mapbox GL -->
         <script src='{!! setting_value('mapbox', 'glScript') !!}'></script>
@@ -16,13 +23,8 @@
 
         @stack('preAppScripts')
 
-        @if (App::environment() != 'production')
-            <script src="{{ asset('js/admin/plugins.js') }}"></script>
-            <script src="{{ asset('js/admin/app.js') }}"></script>
-        @else
-            <script src="{{ asset('js/admin/plugins.min.js') }}"></script>
-            <script src="{{ asset('js/admin/app.min.js') }}"></script>
-        @endif
+        <script src="{{ asset('js/admin/plugins'.$min.'.js') }}"></script>
+        <script src="{{ asset('js/admin/app'.$min.'.js') }}"></script>
 
         @stack('postAppScripts')
 

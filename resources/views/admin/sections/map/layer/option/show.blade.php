12a57b7fb9e7535912ba079740a05b7118a698b8
Jaume Sala <jaumesala@gmail.com>
2016-04-22 23:58:01 +0200

    Fix for new map without layers

--- resources/views/admin/sections/map/layer/option/show.blade.php
+++ resources/views/admin/sections/map/layer/option/show.blade.php
@@ -1,16 +1,27 @@
+<?php
+    $layerId = 0;
+    $optionName = '';
+
+    if(isset($layer))
+    {
+        $layerId = $layer->id;
+        $optionName = $layer->{$option['name']};
+    }
+
+?>
 @if($option['type'] == 'sources')
     <div class="form-group input-group-xs clearfix" data-filter="{{ implode(" ", $option['filter']) }}">
         <div class="col-xs-4">
             <div class="row">
-                <label for="{{ $option['name']."-".$layer->id }}" class="control-label">{{ $option['label'] }}</label>
+                <label for="{{ $option['name']."-".$layerId }}" class="control-label">{{ $option['label'] }}</label>
             </div>
         </div>
         <div class="col-xs-8">
             <div class="row">
                 {!! Form::select($option['name'],
                     $option['list'],
-                    old($option['name'], $layer->{$option['name']}),
-                    [   'id' => $option['name']."-".$layer->id,
+                    old($option['name'], $optionName),
+                    [   'id' => $option['name']."-".$layerId,
                         'class' => 'form-control select2',
                         'data-options' => '{ "containerCssClass" : "select2-container-xs", "dropdownCssClass" : "select2-dropdown-xs"  }' ]
                         ) !!}
@@ -21,15 +32,15 @@
     <div class="form-group input-group-xs clearfix" data-filter="{{ implode(" ", $option['filter']) }}">
         <div class="col-xs-7">
             <div class="row">
-                <label for="{{ $option['name']."-".$layer->id }}" class="control-label">{{ $option['label'] }}</label>
+                <label for="{{ $option['name']."-".$layerId }}" class="control-label">{{ $option['label'] }}</label>
             </div>
         </div>
         <div class="col-xs-5">
             <div class="row">
                 {!! Form::select($option['name'],
                     $option['list'],
-                    old($option['name'], $layer->{$option['name']}),
-                    [   'id' => $option['name']."-".$layer->id,
+                    old($option['name'], $optionName),
+                    [   'id' => $option['name']."-".$layerId,
                         'class' => 'form-control select2',
                         'data-options' => '{ "minimumResultsForSearch": -1, "containerCssClass" : "select2-container-xs", "dropdownCssClass" : "select2-dropdown-xs"  }' ]
                         ) !!}
@@ -40,12 +51,12 @@
     <div class="form-group input-group-xs clearfix" data-filter="{{ implode(" ", $option['filter']) }}">
         <div class="col-xs-7">
             <div class="row">
-                <label for="{{ $option['name']."-".$layer->id }}" class="control-label">{{ $option['label'] }}</label>
+                <label for="{{ $option['name']."-".$layerId }}" class="control-label">{{ $option['label'] }}</label>
             </div>
         </div>
         <div class="col-xs-5">
             <div class="row">
-                <input type="text" class="form-control {{ $option['class'] }}" id="{{ $option['name']."-".$layer->id }}" name="{{ $option['name'] }}" placeholder="{{ $option['placeholder'] }}" value="{{ $layer->{$option['name']} }}">
+                <input type="text" class="form-control {{ $option['class'] }}" id="{{ $option['name']."-".$layerId }}" name="{{ $option['name'] }}" placeholder="{{ $option['placeholder'] }}" value="{{ $optionName }}">
             </div>
         </div>
     </div>

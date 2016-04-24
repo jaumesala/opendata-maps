96b060e427307599c9c5a1bc04e93dfa0eb2839d
Jaume Sala <jaumesala@gmail.com>
2016-04-24 19:31:33 +0200

    Map - Create scope to filter allowed maps per user

--- app/Models/Map.php
+++ app/Models/Map.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Auth;
 
 class Map extends Model
 {
@@ -29,5 +30,43 @@ class Map extends Model
     public function layers()
     {
         return $this->hasMany(Layer::class)->orderBy('order', 'asc');
+    }
+
+    /**
+     * Scope a query to only include allowed maps.
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeAllowed($query)
+    {
+        // superadmin user
+        if(Auth::check()){
+            if(Auth::user()->hasRole('superadmin')){
+                // no filter = all maps
+                return $query;
+            }
+        }
+
+        // Other users
+        $allowed = $query
+            // public maps
+            ->where(function($query){
+                $query->where('visibility', '=', 'public')->where('active', '=', true);
+            });
+
+        if(Auth::check()){
+            $allowed = $allowed
+                // shared maps
+                ->orWhere(function($query){
+                    $query->where('visibility', '=', 'shared')->where('active', '=', true);
+                })
+                // user maps (private)
+                ->orWhere(function($query){
+                    $query->where('user_id', '=', Auth::user()->id);
+                });
+        }
+
+        return $allowed;
+
     }
 }
